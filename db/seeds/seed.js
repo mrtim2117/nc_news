9048a6c08069f4d0922db18fc6b9b2400839f1a7
mrtim2117 <tim.staton@outlook.com>
2021-11-29 13:01:43 +0000

    Database and table creation

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,54 @@
+const db = require("../connection");
+
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
+
   // 1. create tables
+  return db
+    .query(`DROP TABLE IF EXISTS comments;`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS articles;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS topics;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS users;`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE users (
+        username VARCHAR(20) PRIMARY KEY,
+        avatar_url VARCHAR(100),
+        name VARCHAR(50) NOT NULL
+      );`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE topics (
+        slug VARCHAR(30) PRIMARY KEY,
+        description VARCHAR(255)
+      );`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE articles (
+        article_id SERIAL PRIMARY KEY,
+        title VARCHAR (100) NOT NULL,
+        body TEXT,
+        votes INT DEFAULT 0,
+        author VARCHAR(20) REFERENCES users(username),
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+      );`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE comments(
+        comment_id SERIAL PRIMARY KEY,
+        author VARCHAR(20) REFERENCES users(username),
+        article_id INT REFERENCES articles(article_id),
+        votes INT DEFAULT 0,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        body TEXT
+      );`);
+    });
+
   // 2. insert data
 };
 
