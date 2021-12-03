051609f08f19402d99ef47d00080f23a0a5d778e
mrtim2117 <tim.staton@outlook.com>
2021-12-03 20:30:52 +0000

    Tidy up files

--- contollers/bad-path.controller.js
+++
@@ -1,7 +0,0 @@
-const { handleCustomErrors } = require("../errors/errors.js");
-
-const handleInvalidPath = (req, res, next) => {
-  handleCustomErrors({ status: 404, msg: "Path not found!" }, req, res, next);
-};
-
-module.exports = handleInvalidPath;
