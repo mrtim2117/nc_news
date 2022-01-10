f4571a1ff4790cbdac7e78a4da876354811bec1d
mrtim2117 <tim.staton@outlook.com>
2022-01-10 18:12:41 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const apiRouter = require("./routers/api.router");
+const cors = require("cors");
+
 const {
   handleInvalidPath,
   handlePSQLErrors,
@@ -8,6 +10,7 @@ const {
 } = require("./errors/errors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
